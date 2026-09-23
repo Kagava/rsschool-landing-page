8b4a8aadcdfe2b3589a36247aa1d17719484b3c1
Kagava <esspakov@mail.ru>
2026-09-23 17:16:39 +0700

    feat: add menu tags functional

--- coffee-house/scripts/menu.js
+++ coffee-house/scripts/menu.js
@@ -1,6 +1,8 @@
 import { getProducts } from "./get-product.js";
 import { ProductCard } from "./product-card.js";
 
+const menuTags = document.querySelectorAll(".menu__tag");
+
 const cardsContainer = document.querySelector(".main__menu-container");
 
 const coffeeArray = [];
@@ -24,12 +26,52 @@ getProducts().then((data) => {
         break;
     }
   }
-  createView();
+  createView("coffee");
 });
 
-function createView() {
-  coffeeArray.forEach((item, index, array) => {
-    const newCard = new ProductCard(item);
-    cardsContainer.append(newCard.getCard());
+function createView(activeMenu) {
+  console.log(activeMenu);
+  switch (activeMenu) {
+    case "coffee":
+      cardsContainer.innerHTML = "";
+      coffeeArray.forEach((item, index, array) => {
+        const newCard = new ProductCard(item);
+        cardsContainer.append(newCard.getCard());
+      });
+      break;
+    case "tea":
+      cardsContainer.innerHTML = "";
+      teaArray.forEach((item, index, array) => {
+        const newCard = new ProductCard(item);
+        cardsContainer.append(newCard.getCard());
+      });
+      break;
+    case "desert":
+      cardsContainer.innerHTML = "";
+      dessertArray.forEach((item, index, array) => {
+        const newCard = new ProductCard(item);
+        cardsContainer.append(newCard.getCard());
+      });
+      break;
+    default:
+      console.log("ERROR");
+      break;
+  }
+}
+
+menuTags.forEach((item, index, array) => {
+  item.addEventListener("click", tagEvent);
+});
+
+function tagEvent(e) {
+  const target = e.target;
+  if (target.classList.contains("tag_active")) {
+    return;
+  }
+  menuTags.forEach((item) => {
+    item.classList.remove("tag_active");
   });
+  target.classList.add("tag_active");
+
+  createView(target.classList[1].split("__")[1]);
 }
